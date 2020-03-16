b155a66f1f8f1e9041adeb0bb399d455c28bc5e3
Mykola Kozlovskiy <mathematic204cgnu@gmail.com>
2020-03-16 11:37:59 +0200

    1.1

--- Operations.kt
+++ Operations.kt
@@ -3,13 +3,14 @@ import kotlin.math.max
 class Operations(val st1:String, val st2: String) {
 
 
-    fun sumStrings(): CharArray{
+
+
+    fun sumStrings(): String{
         val n = st1.length; val m = st2.length
         var N = max(n,m)
-        var i = 0; var j = 0
-        var z: Array<IntArray> = Array( 3, {IntArray(N+1,{0}) } )
+        var z= Array( 3, {IntArray(N+1,{0}) } )
         for (i in 0..n-1)
-                z[1][i] = charToInt(st1[n-1-i])
+            z[1][i] = charToInt(st1[n-1-i])
         for (i in 0..m-1)
             z[2][i] = charToInt(st2[m-1-i])
         var t = 0
@@ -25,20 +26,21 @@ class Operations(val st1:String, val st2: String) {
             z[0][i] = z[1][i] + z[2][i] + t
             t = 0
         }
-        var s:CharArray = CharArray(N, {'0'})
+        var s = ""
         for (i in 0 until N)
-            s[i] = intToChar(z[0][N-1-i])
+            s += intToString(z[0][N - 1 - i])
 
-    return s}
-
-
-
+        return s
+    }
 
 
 
 
 
-    fun charToInt(c: Char):Int {
+
+
+
+   private fun charToInt(c: Char):Int {
         val x = c.toInt()
         when (x){
             48 -> return 0
@@ -55,19 +57,19 @@ class Operations(val st1:String, val st2: String) {
         }
     }
 
-    fun intToChar(i: Int):Char{
+    private fun intToString(i: Int):String{
         when (i){
-            0 -> return '0'
-            1 -> return '1'
-            2 -> return '2'
-            3 -> return '3'
-            4 -> return '4'
-            5 -> return '5'
-            6 -> return '6'
-            7 -> return '7'
-            8 -> return '8'
-            9 -> return '9'
-            else -> return 'z'
+            0 -> return "0"
+            1 -> return "1"
+            2 -> return "2"
+            3 -> return "3"
+            4 -> return "4"
+            5 -> return "5"
+            6 -> return "6"
+            7 -> return "7"
+            8 -> return "8"
+            9 -> return "9"
+            else -> return "Not number"
         }
 
     }
--- main.kt
+++ main.kt
@@ -1,11 +1,10 @@
-import kotlin.math.max
 
 fun main(){
 
 
-    val s1: String = readLine()!!
+    val s1= readLine()!!
     val s2 = readLine()!!
-    val x: Operations = Operations(s1,s2)
+    val x = Operations(s1,s2)
     println(x.sumStrings())
 
 
@@ -13,19 +12,3 @@ fun main(){
 }
 
 
-fun charToInt(c: Char):Short {
-    val x = c.toInt()
-    when (x){
-        48 -> return 0
-        49 -> return 1
-        50 -> return 2
-        51 -> return 3
-        52 -> return 4
-        53 -> return 5
-        54 -> return 6
-        55 -> return 7
-        56 -> return 8
-        57 -> return 9
-        else -> return -1
-    }
-}
